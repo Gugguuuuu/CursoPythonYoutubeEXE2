0d90c88c99bc4bfa2366f6f74e4dfac3166f901c
Gugguuuuu <gustavo2011oliveira.lima@gmail.com>
2024-01-22 19:00:36 -0300

    35

---
+++ exerciciosPython/exe33.py
@@ -0,0 +1,13 @@
+#compras = float(input('Digite o valor da sua {}° compra : '.format(i)))
+ask = str(input('Você vai fazer compras? '.strip().lower()))
+if ask == 's' or ask == 'sim':
+   i = 1
+   compras = float(input('Digite o valor da sua {}° compra : '.format(i)))
+   continuar = str(input('Você fez mais compras? '))
+   if continuar == 'sim' or continuar == 's':
+      continuar == True
+while continuar == True:
+   i = 1 + 1
+   compras = float(input('Digite o valor de {}° compra'.format(i)))
+   media = compras
+print(compras)
