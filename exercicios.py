47c342850f97ecf8078c5fa7345ef6076162bb37
Gugguuuuu <gustavo2011oliveira.lima@gmail.com>
2024-01-23 17:03:54 -0300

    finalizado 1

--- exercicios.py
+++ exercicios.py
@@ -1,8 +1,12 @@
-import pyautogui
-import time
-i = 2
-for i in range(25):
-    i = i + 1
-    pyautogui.click(x=155, y=92)
-    pyautogui.write('exe{}.py'.format(i))
-    pyautogui.press('enter')
+# import pyautogui
+# import time
+# i = 2
+# for i in range(25):
+#     i = i + 1
+#     pyautogui.click(x=155, y=92)
+#     pyautogui.write('exe{}.py'.format(i))
+#     pyautogui.press('enter')
+lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+for i, n in enumerate(lista):
+    print(i+1, n)
+    
