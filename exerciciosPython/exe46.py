cf4597dbc14f294220b3aedaa053ee677165cf45
Gugguuuuu <gustavo2011oliveira.lima@gmail.com>
2024-01-24 15:24:26 -0300

    finaliza16

---
+++ exerciciosPython/exe46.py
@@ -0,0 +1,6 @@
+t = int(input('Digites quantos segundo ate acabar o ano novo : '))
+from time import sleep
+for i in range(t+1, 0, -1):
+    print(i, end='\r')
+    sleep(1)
+print('BUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUMMMMMMMMMMM\n'*10)
